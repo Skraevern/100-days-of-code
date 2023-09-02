4e4b6116cab2e3dfc213531d88a56caa70b64d0e
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-02 22:23:29 +0200

    Added 09

---
+++ 01-day-01/09-string-manipilation-and-code-intelligence.py
@@ -0,0 +1,24 @@
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+#Hello World!
+#Hello World!
+#Hello World!
+
+print("")
+
+print("Hello World!\nHello World!\nHello World!")
+#Hello World!
+#Hello World!
+#Hello World!
+
+print("")
+
+print("Hello" + "Angela")
+#HelloAngela
+
+print("Hello" + " Angela")
+#Hello Angela
+
+print("Hello" + " " + "Angela")
+#Hello Angela
