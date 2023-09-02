4377d57a92518ca2e4fcf21467286417a8a5742f
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-02 22:56:01 +0200

    Added 11

---
+++ 01-day-01/11-the-puthon-input-function.py
@@ -0,0 +1,12 @@
+#input("What is your name?\n")
+# What is your name
+#
+
+#Kprint("Hello " + input("What is your name?\n") + "!")
+# What is your name?
+# Kristian
+# Hello Kristian!
+
+print("First Name: " + input("First Name?\n") + "\n" + "Last Name:  " + input("Last name?\n"))
+# First Name: Kristian
+# Last Name:  Skreosen
