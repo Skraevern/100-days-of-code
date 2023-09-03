dd94eaa2ecbaeff046b51638f3ddb16593c4eefe
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-03 13:13:13 +0200

    Added 024 and 025

---
+++ 002-day-002/024-number-manipulations.py
@@ -0,0 +1,14 @@
+print(8 / 3) # 2.6666666666666665
+print(int(8 / 3)) # 2
+print(round(8 / 3, 2)) # 2,67
+
+print(8 // 3) # 2 Flodevision
+print(type(8 // 3)) # <class 'int'>
+
+result = 4 / 2
+print(result) # 2
+result = result + 1 # 3
+print(result)
+result += 1 #4
+print(result) #4
+
