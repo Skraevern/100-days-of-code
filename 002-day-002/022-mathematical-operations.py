aec8c27e9722d382b41a59d49c8608528d6c8388
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-03 12:45:27 +0200

    Added 023

---
+++ 002-day-002/022-mathematical-operations.py
@@ -0,0 +1,18 @@
+print(3 + 5) # 8
+print(7 - 4) # 3
+print(3 * 2) # 5
+print(6 / 3) # 2.0
+print(type(6 / 3)) # <class 'float'>
+print(int(6 / 3)) # 2
+print(type(int(6 / 3))) # <class 'int'>
+print(2 ** 2) # 4
+print(2 ** 3) # 8   2 * 2 * 2
+
+
+# Pemdas 
+# ()
+# **
+# * / 
+# + -
+print(int(3 * 3 + 3 / 3 - 3)) # 7
+print(int(3 * (3 + 3) / 3 - 3)) # 3
