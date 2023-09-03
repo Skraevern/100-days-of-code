98091cb07af4f16b3c5795ae4a7e3b9e19ea5c17
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-03 20:15:47 +0200

    Added 029 and 030

---
+++ 003-day-003/029-if-else-conditional.py
@@ -0,0 +1,14 @@
+print("Welcome to the rollercoaster")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
+# >  Greater than
+# <  Less than
+# >= Greater or equal to
+# <= Lesser than or equal to
+# == Equal to
+# != Not equal to
