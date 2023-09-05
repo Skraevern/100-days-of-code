b449dceda8653c7ec002405dd0de190a33090470
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-05 19:19:33 +0200

    Added 043 list

--- 004-day-004/043-list.py
+++ 004-day-004/043-list.py
@@ -1,2 +1,12 @@
-states_of_america = ["Delaware", "Pennsylvania", "Aapaoskd", "Dsaosd"]
-print(states_of_america[0]) 
+states_of_america = ["Delaware", "Pennsylvania", "Aapaoskd", "Hawaii"]
+print(states_of_america[0]) # Delaware
+print(states_of_america[-1]) # Hawaii
+
+states_of_america[2] = "New Jersey"
+print(states_of_america) # ['Delaware', 'Pennsylvania', 'New Jersey', 'Hawaii']
+
+states_of_america.append("Georgia")
+print(states_of_america) # ['Delaware', 'Pennsylvania', 'New Jersey', 'Hawaii', 'Georgia']
+
+states_of_america.extend(["Minnesota", "California"])
+print(states_of_america) # ['Delaware', 'Pennsylvania', 'New Jersey', 'Hawaii', 'Georgia', 'Minnesota', 'California']
