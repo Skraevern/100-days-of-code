26a89443e270e0f239fd860ef44c7d68145812b5
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-06 20:56:57 +0200

    Added 053 and 055

---
+++ 005-day-005/053-loop-range.py
@@ -0,0 +1,12 @@
+for number in range(1, 10):
+    print(number) # 1 2 3 4 ... 9
+
+
+for number in range(1, 11, 3):
+    print(number) # 1 4 7 10
+
+total = 0
+for number in range(1, 101):
+    total += number
+
+print(number) # 550
