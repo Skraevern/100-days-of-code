84472feffa98884d3643991bbc50e6bdbf74988f
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-07 21:47:48 +0200

    added 062 and 063

---
+++ 006-day-006/062-while-loop.py
@@ -0,0 +1,12 @@
+list_of_items = ["item1", "item2", "item3"]
+for item in list_of_items:
+    print(item)
+
+for number in range(0, 10):
+    print(number)
+
+# While
+number_of_hurdles = 6
+while number_of_hurdles > 0:
+    print(number_of_hurdles)
+    number_of_hurdles -= 1
