d9400b92ef550a99adcd2f707cc3316be931056e
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-08 22:11:33 +0200

    064

---
+++ 006-day-006/064-hurdle-variable-hight.py
@@ -0,0 +1,27 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+
+"""
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    while right_is_clear() == False:
+        move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    while front_is_clear():
+        move()
+    turn_left()
+    
+
+while at_goal() == False:
+    if front_is_clear():
+        move()
+    else:
+        jump()
+"""
