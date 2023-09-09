2284201ba4e469dda3af0cddd2a97b66427850cf
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-09 20:14:24 +0200

    075 and stages

---
+++ 007-day-007/075-challange-4.py
@@ -0,0 +1,37 @@
+import random
+import stages
+
+word_list = ["ardvark", "baboon", "camel"]
+solution_word = random.choice(word_list)
+display = []
+end_game = False
+lives = 6
+for letter in solution_word:
+        display.append("_")
+
+def print_game():
+    print(stages.stages[lives])
+    print(f"{' '.join(display)}") #Join all the elements in the list and turn it into a String.
+     
+print(solution_word)
+print_game()
+
+while end_game == False:
+    guess = input("Guess a letter: ").lower()
+    if len(guess) == 1:
+        for i in range(0, len(solution_word)): #checks if guessed letter is in word
+            if guess == solution_word[i]:
+                display[i] = guess
+        if guess not in solution_word:
+             lives -= 1
+
+        print_game()
+
+        if "_" not in display:
+             end_game = True
+             print("You win!")
+        if lives == 0:
+             end_game = True
+             print("You loose...")
+    else:
+        print("Please enter one letter.")
