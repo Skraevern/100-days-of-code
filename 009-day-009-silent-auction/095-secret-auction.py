4e706b52fddad36b7bb6dc097dba5120efd65cd4
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-10 22:03:09 +0200

    095

---
+++ 009-day-009-silent-auction/095-secret-auction.py
@@ -0,0 +1,43 @@
+import os
+import art
+
+bidding_list = []
+keep_bidding = True
+winner = ""
+winning_bid = 0
+
+def bidders():
+    bidder = {}
+    bidder["name"] = input("What is your name? ")
+    bidder["bid"] = int(input("What's tour bid: $"))
+    bidding_list.append(bidder)
+
+def check_winner():
+    global winning_bid, winner
+    for i in range(0, len(bidding_list)):
+        if int(bidding_list[i]["bid"]) > int(winning_bid):
+            winning_bid = bidding_list[i]["bid"]
+            winner = bidding_list[i]["name"]
+
+
+os.system("cls||clear")
+print(art.logo)
+print("Welcome to the secret auction program.")
+
+while keep_bidding == True:
+    bidders()
+    more_bidders = input('Are there any other bidders? Type "yes" or "no": ').lower()
+    os.system("cls||clear")
+    if more_bidders == "no":
+        keep_bidding = False
+
+check_winner()
+
+print(f"The winner is {winner} with a bid of ${winning_bid}")
+
+
+
+
+    
+
+
