80c77633ffc8adb4251c2523d215b58432a9c202
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-10 13:42:02 +0200

    added main.py

--- 007-day-007-hangman/075-challange-4.py
+++ 007-day-007-hangman/075-challange-4.py
@@ -1,5 +1,5 @@
 import random
-import stages
+import hangman_art
 import hangman_words
 
 word_list = hangman_words.words
@@ -8,31 +8,35 @@ display = []
 end_game = False
 lives = 6
 for letter in solution_word:
-        display.append("_")
+    display.append("_")
+
 
 def print_game():
-    print(stages.stages[lives])
-    print(f"{' '.join(display)}") #Join all the elements in the list and turn it into a String.
-     
+    print(hangman_art.stages[lives])
+    print(
+        f"{' '.join(display)}"
+    )  # Join all the elements in the list and turn it into a String.
+
+
 print(solution_word)
 print_game()
 
 while end_game == False:
     guess = input("Guess a letter: ").lower()
     if len(guess) == 1:
-        for i in range(0, len(solution_word)): #checks if guessed letter is in word
+        for i in range(0, len(solution_word)):  # checks if guessed letter is in word
             if guess == solution_word[i]:
                 display[i] = guess
         if guess not in solution_word:
-             lives -= 1
+            lives -= 1
 
         print_game()
 
         if "_" not in display:
-             end_game = True
-             print("You win!")
+            end_game = True
+            print("You win!")
         if lives == 0:
-             end_game = True
-             print("You loose...")
+            end_game = True
+            print("You loose...")
     else:
         print("Please enter one letter.")
---
+++ 007-day-007-hangman/main.py
@@ -0,0 +1,48 @@
+import random
+import hangman_art
+import hangman_words
+
+word_list = hangman_words.words
+solution_word = random.choice(word_list)
+already_guessed = []
+display = []
+end_game = False
+lives = 6
+for letter in solution_word:
+    display.append("_")
+
+
+def print_game():
+    print(hangman_art.stages[lives])
+    print(
+        f"{' '.join(display)}"
+    )  # Join all the elements in the list and turn it into a String.
+
+
+print(hangman_art.logo)
+# print(f"------   Solution word is {solution_word}   ------")
+print_game()
+
+while end_game == False:
+    guess = input("Guess a letter: ").lower()
+    if len(guess) == 1:
+        if guess in already_guessed:
+            print(f"You have already guessed {guess}. Try another letter.")
+        else:
+            already_guessed.append(guess)
+            for i in range(0, len(solution_word)):  # in solution_word?
+                if guess == solution_word[i]:
+                    display[i] = guess  # replace "_" with guess
+            if guess not in solution_word:
+                lives -= 1
+
+        print_game()
+
+        if "_" not in display:
+            end_game = True
+            print("You win!")
+        if lives == 0:
+            end_game = True
+            print("You loose...")
+    else:
+        print("Please enter one letter.")
