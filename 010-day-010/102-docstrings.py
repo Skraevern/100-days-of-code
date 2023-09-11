93ecb9ca9c57ddc6348690580a00bb8a2c2d6dfb
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-11 18:46:46 +0200

    102

---
+++ 010-day-010/102-docstrings.py
@@ -0,0 +1,8 @@
+def format_name(f_name, l_name):
+    """Take a first and last name and format it to return the title case case version of the name"""
+    if f_name == "" or l_name == "":
+        return  # Escapes the function
+    return f_name.title() + " " + l_name.title()
+
+
+print(format_name("kristian", "skreosen"))
