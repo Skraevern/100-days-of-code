10e0e59784a67f3f7cf0d28654abcfd63549b553
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-11 17:07:38 +0200

    099

---
+++ 010-day-010/099-functions-with-output.py
@@ -0,0 +1,7 @@
+def format_name(f_name, l_name):
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+
+    return f"{formated_f_name} {formated_l_name}"
+   #return f_name.title() + " " + l_name.title()
+print(format_name("kristian", "skreosen"))
