aadf61816f4f59c7ef677a8888f1979b56a1ce3e
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-12 22:39:15 +0200

    changed int to float

--- 010-day-010/main.py
+++ 010-day-010/main.py
@@ -16,18 +16,18 @@ def calculator():
     """Asks for input, calculates inputs, repeats"""
     repeat_calculator = True
 
-    num1 = int(input("What's the first number?: "))
+    num1 = float(input("What's the first number?: "))
     for symbol in operations:
         print(symbol)
     while repeat_calculator:
         operation_symbol = input(f"Pick an operation.\n{num1} ")
         calculation_function = operations[operation_symbol]
-        num2 = int(input(f"What's the second number?\n{num1} {operation_symbol} "))
+        num2 = float(input(f"What's the second number?\n{num1} {operation_symbol} "))
 
         answer = calculation_function(num1, num2)
         print(f"{num1} {operation_symbol} {num2} = {answer}")
         
-        if input(f'Type "y" to continue calculating with {answer}, press any key to restart. "control" + "c" to quit: ') != "y":
+        if input(f'Type "y" to continue calculating with {answer}, press any key to restart. "control + c" to quit: ') != "y":
             repeat_calculator = False
             calculator()
         num1 = answer
