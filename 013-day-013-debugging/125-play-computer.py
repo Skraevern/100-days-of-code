0a59c4c0e90ec99444c344adc8fbee5d756b5809
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-14 18:22:52 +0200

    125

--- 013-day-013-debugging/125-play-computer.py
+++ 013-day-013-debugging/125-play-computer.py
@@ -1,6 +1,6 @@
-# # Play Computer
-# year = int(input("What's your year of birth?"))
-# if year > 1980 and year < 1994:
-#   print("You are a millenial.")
-# elif year > 1994:
-#   print("You are a Gen Z.")
+# Play Computer
+year = int(input("What's your year of birth?")) #1994 == næthing
+if year > 1980 and year <= 1994: #if year > 1980 and year < 1994:
+  print("You are a millenial.")
+elif year > 1994:
+  print("You are a Gen Z.")
