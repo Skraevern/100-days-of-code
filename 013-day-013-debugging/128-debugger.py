5bdcd058c2500fd3374eebc343cb60217dfa8881
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-14 19:53:09 +0200

    130

--- 013-day-013-debugging/128-debugger.py
+++ 013-day-013-debugging/128-debugger.py
@@ -1,9 +1,13 @@
-# #Use a Debugger
-# def mutate(a_list):
-#   b_list = []
-#   for item in a_list:
-#     new_item = item * 2
-#   b_list.append(new_item)
-#   print(b_list)
+#Use a Debugger
 
-# mutate([1,2,3,5,8,13])
+# https://pythontutor.com/render.html#mode=display
+
+def mutate(a_list):
+    b_list = []
+    for item in a_list:
+        new_item = item * 2
+        b_list.append(new_item) ## Missing indentation
+    print(b_list)
+
+mutate([1,2,3,5,8,13])
+
