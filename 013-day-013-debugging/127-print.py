dd5f0bf428c5cdb8981bf272fb9e6e75f7a66c5f
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-14 18:26:20 +0200

    127

--- 013-day-013-debugging/127-print.py
+++ 013-day-013-debugging/127-print.py
@@ -1,8 +1,8 @@
 
-# #Print is Your Friend
-# pages = 0
-# word_per_page = 0
-# pages = int(input("Number of pages: "))
-# word_per_page == int(input("Number of words per page: "))
-# total_words = pages * word_per_page
-# print(total_words)
+#Print is Your Friend
+pages = 0
+word_per_page = 0
+pages = int(input("Number of pages: "))
+word_per_page == int(input("Number of words per page: ")) # ==
+total_words = pages * word_per_page
+print(total_words)
