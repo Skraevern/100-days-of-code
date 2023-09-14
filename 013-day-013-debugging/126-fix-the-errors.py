62704dc3a24bc089feb4793dedbd9cca34c74f3c
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-14 18:24:30 +0200

    126

--- 013-day-013-debugging/126-fix-the-errors.py
+++ 013-day-013-debugging/126-fix-the-errors.py
@@ -1,4 +1,4 @@
-# # Fix the Errors
-# age = input("How old are you?")
-# if age > 18:
-# print("You can drive at age {age}.")
+# Fix the Errors
+age = int(input("How old are you? ")) # missing int()
+if age > 18:
+    print("You can drive at age {age}.") # Missing indentation
