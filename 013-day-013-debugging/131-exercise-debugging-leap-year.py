d0bf7d9da0ce603dff1d404dda0ce1f414253083
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-14 20:03:05 +0200

    132

--- 013-day-013-debugging/131-exercise-debugging-leap-year.py
+++ 013-day-013-debugging/131-exercise-debugging-leap-year.py
@@ -5,7 +5,7 @@
 # No shortcuts - don't copy-paste to replace the code entirely with a working solution.
 # Fix the code so that it works and when you hit submit it should pass all the tests.
 
-year = int(input("Which year do you want to check?"))
+year = int(input("Which year do you want to check?")) # missing int()
 
 if year % 4 == 0:
   if year % 100 == 0:
