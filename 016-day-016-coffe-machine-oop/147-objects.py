a93e17c6f4f799bdf65b975147b05bb2cde8a286
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-17 12:17:25 +0200

    147

---
+++ 016-day-016-coffe-machine-oop/147-objects.py
@@ -0,0 +1,13 @@
+import turtle
+
+timmy = turtle.Turtle()
+
+print(timmy)
+
+timmy.shape("turtle")
+timmy.color("red")
+timmy.forward(100)
+
+my_screen = turtle.Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
