0edf8b7ee83d8e230e672586848295cc94baed2e
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-18 09:02:14 +0200

    Added 5 quiz categories

--- 017-day-017-quiz/main.py
+++ 017-day-017-quiz/main.py
@@ -7,17 +7,26 @@ os.system("cls||clear")
 
 question_bank = []
 
-for i in range(0, len(question_data)):
-    question = Question(
-        question_data[i]["question"], question_data[i]["correct_answer"]
-    )
-    question_bank.append(question)
+for key in question_data:
+    print(key)
+category = input("Type in quiz category: ").title()
 
-print(question_bank[0].answer)
-quiz = QuizBrain(question_bank)
+print(len(question_data[category]))
 
-while quiz.still_has_questions():
-    quiz.next_question()
+if category in question_data:
+    for i in range(0, len(question_data[category])):
+        question = Question(
+            question_data[category][i]["question"],
+            question_data[category][i]["correct_answer"],
+        )
+        question_bank.append(question)
 
-print("You've completed the quiz!")
-print(f"Your final score was: {quiz.correct_answers}/{quiz.question_number}")
+    quiz = QuizBrain(question_bank)
+
+    while quiz.still_has_questions():
+        quiz.next_question()
+
+    print("You've completed the quiz!")
+    print(f"Your final score was: {quiz.correct_answers}/{quiz.question_number}")
+else:
+    print("Wrong input")
