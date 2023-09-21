e34cbb3f7b157eef6e02ff8ea53e4ab3167fba70
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-21 12:51:50 +0200

    Added turtle as food. Added random tilt angle for turtle

--- 021-day-021-snake-part-2/food.py
+++ 021-day-021-snake-part-2/food.py
@@ -7,9 +7,9 @@ FOOD_COLOR = (111, 209, 3)
 class Food(Turtle):
     def __init__(self) -> None:
         super().__init__()
-        self.shape("circle")
+        self.shape("turtle")
         self.penup()
-        self.shapesize(stretch_len=0.5, stretch_wid=0.5)  # 10 * 10
+        self.shapesize(stretch_len=0.7, stretch_wid=0.7)  # 10 * 10
         self.color(FOOD_COLOR)
         self.speed("fastest")
         self.refresh()
@@ -17,4 +17,6 @@ class Food(Turtle):
     def refresh(self):
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
+        random_angle = random.randint(0, 360)
         self.goto(random_x, random_y)
+        self.tilt(random_angle)
