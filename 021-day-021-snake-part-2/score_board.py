b072cb23159fdd593d43defc51a54f58e1ffedff
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-24 17:42:47 +0200

    Added uppercase to score_board

--- 021-day-021-snake-part-2/score_board.py
+++ 021-day-021-snake-part-2/score_board.py
@@ -11,16 +11,16 @@ class ScoreBoard(Turtle):
         self.penup()
         self.goto(0, 270)
         self.color("white")
-        self.write(f"Score: {self.score}", font=FONT, align=ALIGNMENT)
+        self.write(f"SCORE: {self.score}", font=FONT, align=ALIGNMENT)
         self.hideturtle()
 
     def increase_score(self):
         self.score += 1
         self.clear()
-        self.write(f"Score: {self.score}", font=FONT, align=ALIGNMENT)
+        self.write(f"SCORE: {self.score}", font=FONT, align=ALIGNMENT)
 
     def game_over(self):
         self.clear()
         self.goto(0, 0)
         self.color("white")
-        self.write("Game over!", font=("Courier", 40, "normal"), align=ALIGNMENT)
+        self.write("GAME OVER", font=("Courier", 40, "normal"), align=ALIGNMENT)
