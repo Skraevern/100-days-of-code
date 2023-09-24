cfd0d63dc56fa4261c246216a8cc8b6b780f69e9
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-24 21:49:41 +0200

    a

--- 025-day-025-CSV-data/main.py
+++ 025-day-025-CSV-data/main.py
@@ -23,3 +23,11 @@ with open("./weather_data.csv") as file:
 
 csv_data = pandas.read_csv("./weather_data.csv")
 print(csv_data)
+#          day  temp condition
+# 0     Monday    12     Sunny
+# 1    Tuesday    14      Rain
+# 2  Wednesday    15      Rain
+# 3   Thursday    14    Cloudy
+# 4     Friday    21     Sunny
+# 5   Saturday    22     Sunny
+# 6     Sunday    24     Sunny
