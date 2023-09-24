fd885f310c675949ba8c1af79355c67ce97220c0
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-09-24 18:42:57 +0200

    Added high score write to file in snake

--- 021-day-021-snake-part-2/score_board.py
+++ 021-day-021-snake-part-2/score_board.py
@@ -8,7 +8,8 @@ class ScoreBoard(Turtle):
     def __init__(self) -> None:
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("high_score.txt") as file:
+            self.high_score = int(file.read())
         self.penup()
         self.goto(0, 270)
         self.color("white")
@@ -30,6 +31,9 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("high_score.txt", mode="w") as file:
+                file.write(f"{self.high_score}")
+
         self.score = 0
         self.update_scoreboard()
 
