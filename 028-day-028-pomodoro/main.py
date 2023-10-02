bd35bcb1702c6926cf253fbd03cd05368cd75d67
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-10-02 09:05:45 +0200

    258

--- 028-day-028-pomodoro/main.py
+++ 028-day-028-pomodoro/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -9,23 +10,62 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+CHECK_MARK = "✔"
+
 
 # ---------------------------- TIMER RESET ------------------------------- #
+def reset():
+    pass
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
+def start():
+    count_down(WORK_MIN * 60)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_min < 10:
+        count_min = f"0{count_min}"
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+# Column 1
+start_btn = Button(text="Start", command=start)
+start_btn.grid(column=1, row=3)
+
+# Column 2
+timer_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 40, "bold"))
+timer_label.grid(column=2, row=1)
 
 tomato_img = PhotoImage(file="./tomato.png")
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 canvas.create_image(100, 112, image=tomato_img)
-canvas.pack()
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(
+    100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold")
+)
+canvas.grid(column=2, row=2)
+
+check_mark_str = CHECK_MARK
+check_label = Label(
+    text=check_mark_str, bg=YELLOW, fg=GREEN, font=(FONT_NAME, 30, "normal")
+)
+check_label.grid(column=2, row=4)
+
+# Column 3
+reset_btn = Button(text="Reset", command=reset)
+reset_btn.grid(column=3, row=3)
+
 
 window.mainloop()
