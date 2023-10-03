3f4438556e535e4814e5c7706d485291b2af4072
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-10-03 14:53:35 +0200

    Added save psw function

--- 029-day-029-password-manager/main.py
+++ 029-day-029-password-manager/main.py
@@ -2,7 +2,17 @@ from tkinter import *
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_psw():
+    with open("passwords.txt", mode="a") as file:
+        file.write(
+            f"\n{website_input.get()} | {email_input.get()} | {password_output.get()}"
+        )
+    website_input.delete(0, END)
+    password_output.delete(0, END)
+    website_input.focus()
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -28,16 +38,17 @@ canvas.grid(column=2, row=1)
 website_input = Entry(width=45)
 website_input.insert(END, string="")
 website_input.grid(column=2, row=2, columnspan=2, sticky="w")
+website_input.focus()  # Start courser at
 
 email_input = Entry(width=45)
-email_input.insert(END, string="")
+email_input.insert(END, string="standard.email@icloud.com")
 email_input.grid(column=2, row=3, columnspan=2, sticky="w")
 
 password_output = Entry(width=25)
 password_output.insert(END, string="")
 password_output.grid(column=2, row=4, sticky="w")
 
-add_btn = Button(text="Add", width=42)
+add_btn = Button(text="Add", width=42, command=save_psw)
 add_btn.grid(column=2, row=5, columnspan=2, sticky="w")
 
 # Column 3
