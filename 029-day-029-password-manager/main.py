dae163a43d8cd3851a69e0883929d7b2c87a09fb
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-10-03 10:09:24 +0200

    Finished UI

--- 029-day-029-password-manager/main.py
+++ 029-day-029-password-manager/main.py
@@ -17,19 +17,19 @@ email_label = Label(text="Email/Username:")
 email_label.grid(column=1, row=3)
 
 password_label = Label(text="Password:")
-password_label.grid(column=1, rows=4)
+password_label.grid(column=1, row=4)
 
 # Column 2
 img = PhotoImage(file=("./logo.png"))
 canvas = Canvas(width=200, height=200)
-canvas.create_image(100, 100, image=img)
+canvas.create_image(125, 100, image=img)
 canvas.grid(column=2, row=1)
 
-website_input = Entry(width=46)
+website_input = Entry(width=45)
 website_input.insert(END, string="")
 website_input.grid(column=2, row=2, columnspan=2, sticky="w")
 
-email_input = Entry(width=46)
+email_input = Entry(width=45)
 email_input.insert(END, string="")
 email_input.grid(column=2, row=3, columnspan=2, sticky="w")
 
@@ -37,8 +37,8 @@ password_output = Entry(width=25)
 password_output.insert(END, string="")
 password_output.grid(column=2, row=4, sticky="w")
 
-add_btn = Button(text="Add")
-# add_btn.grid(column=2, row=5)
+add_btn = Button(text="Add", width=42)
+add_btn.grid(column=2, row=5, columnspan=2, sticky="w")
 
 # Column 3
 gen_psw_btn = Button(text="Generate Password")
