68b00643c3bf3fc6a2d91eb1ffdf93d74da803f1
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-10-05 14:41:44 +0200

    Added flip cards

--- 031-day-031-flash-card-program/main.py
+++ 031-day-031-flash-card-program/main.py
@@ -5,10 +5,28 @@ import random
 BACKGROUND_COLOR = "#B1DDC6"
 
 
+# --- Canvas config --- #
+def canvas_config(word, language, image, color):
+    canvas.itemconfig(canvas_image, image=image)
+    canvas.itemconfig(language_text, text=language, fill=color)
+    canvas.itemconfig(word_text, text=word, fill=color)
+
+
+# --- Flip Card --- #
+def flip_card():
+    global random_index
+    english_word = data_dict[random_index]["English"]
+    canvas_config(english_word, "English", card_back_img, "white")
+
+
 # ---Change words --- #
 def change_words():
-    random_word = data_dict[random.randint(0, len(data_dict) - 1)]["French"]
-    canvas.itemconfig(word_text, text=random_word)
+    global random_index, flip_card_timer
+    window.after_cancel(flip_card_timer)
+    random_index = random.randint(0, len(data_dict) - 1)
+    french_word = data_dict[random_index]["French"]
+    canvas_config(french_word, "French", card_front_img, "black")
+    flip_card_timer = window.after(3000, func=flip_card)
 
 
 # ---Data--- #
@@ -21,11 +39,13 @@ window = Tk()
 window.title("Flash Card Language Learner")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_card_timer = window.after(3000, func=flip_card)
+
 # Canvas
 card_front_img = PhotoImage(file="./images/card_front.png")
 card_back_img = PhotoImage(file="./images/card_back.png")
 canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
-canvas.create_image(400, 263, image=card_front_img)
+canvas_image = canvas.create_image(400, 263, image=card_front_img)
 canvas.grid(column=1, row=1, columnspan=2)
 language_text = canvas.create_text(
     400, 150, text="Title", fill="black", font=("Ariel", 40, "italic")
