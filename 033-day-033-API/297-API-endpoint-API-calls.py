4844aa8d576bb45bcb589f8814c01dfd107f3903
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-10-06 19:47:46 +0200

    297

---
+++ 033-day-033-API/297-API-endpoint-API-calls.py
@@ -0,0 +1,26 @@
+import requests
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+print(response)  # <Response [200]>
+print(response.status_code)  # 200
+response.raise_for_status()
+
+fault_response = requests.get(url="http://api.open-notify.org/issssssssssss-now.json")
+print(response)  # <Response [404]>
+print(response.status_code)  # 404
+# fault_response.raise_for_status()  # requests.exceptions.HTTPError: 404 Client Error: Not Found for url:......
+
+data = response.json()
+print(data)
+# {'iss_position': {'longitude': '-131.9207', 'latitude': '14.9151'}, 'timestamp': 1696613996, 'message': 'success'}
+
+print(data["iss_position"])
+# {'longitude': '-129.8320', 'latitude': '17.5348'}
+
+print(data)
+print(data["iss_position"]["longitude"])
+# -127.0720
+
+iss_position = (data["iss_position"]["longitude"], data["iss_position"]["latitude"])
+print(iss_position)
+# ('-121.3263', '27.0343')
