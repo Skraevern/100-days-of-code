e727b4766f6134c7a170b0690a88e765677bb7ed
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-10-11 21:22:29 +0200

    331

--- 038-day-038-workout-tracking/main.py
+++ 038-day-038-workout-tracking/main.py
@@ -1,6 +1,19 @@
 import os
+import requests
 
-APP_ID = os.environ.get("NUTRITIONIX_ID")
-API_KEY = os.environ.get("NUTRITIONIX_KEY")
+APP_ID = os.environ.get("NUTRITIONIXID")
+API_KEY = os.environ.get("NUTRITIONIXKEY")
+ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
 print(APP_ID, API_KEY)
+
+headers = {
+    "Content-Type": "application/json",
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+    "x-remote-user-id": "0",
+}
+query = {"query": input("Tell me what exercise you did: ")}
+
+response = requests.post(url=ENDPOINT, json=query, headers=headers)
+print(response.text)
