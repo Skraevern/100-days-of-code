5c40ad6484f6d3c2f01c3bfdafafb1d597b33f39
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-10-17 11:53:28 +0200

    372

---
+++ 045-day-045-web-scraping/372-parsing-html.py
@@ -0,0 +1,96 @@
+from bs4 import BeautifulSoup
+
+with open(file="./website.html") as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents, "html.parser")
+
+print(soup.title)
+# <title>Angela's Personal Site</title>
+
+print(soup.title.name)
+# title
+
+print(soup.title.string)
+# Angela's Personal Site
+
+print(soup)
+# <!DOCTYPE html>
+
+# <html>
+# <head>
+# <meta charset="utf-8"/>
+# <title>Angela's Personal Site</title>
+# </head>
+# <body>
+# <h1 id="name">Angela Yu</h1>
+# <p><em>Founder of <strong><a href="https://www.appbrewery.co/">The App Brewery</a></strong>.</em></p>
+# <p>I am an iOS and Web Developer. I ❤️ coffee and motorcycles.</p>
+# <hr/>
+# <h3 class="heading">Books and Teaching</h3>
+# <ul>
+# <li>The Complete iOS App Development Bootcamp</li>
+# <li>The Complete Web Development Bootcamp</li>
+# <li>100 Days of Code - The Complete Python Bootcamp</li>
+# </ul>
+# <hr/>
+# <h3 class="heading">Other Pages</h3>
+# <a href="https://angelabauer.github.io/cv/hobbies.html">My Hobbies</a>
+# <a href="https://angelabauer.github.io/cv/contact-me.html">Contact Me</a>
+# </body>
+# </html>
+
+print(soup.prettify())
+# !DOCTYPE html>
+# <html>
+#  <head>
+#   <meta charset="utf-8"/>
+#   <title>
+#    Angela's Personal Site
+#   </title>
+#  </head>
+#  <body>
+#   <h1 id="name">
+#    Angela Yu
+#   </h1>
+#   <p>
+#    <em>
+#     Founder of
+#     <strong>
+#      <a href="https://www.appbrewery.co/">
+#       The App Brewery
+#      </a>
+#     </strong>
+#     .
+#    </em>
+#   </p>
+#   <p>
+#    I am an iOS and Web Developer. I ❤️ coffee and motorcycles.
+#   </p>
+#   <hr/>
+#   <h3 class="heading">
+#    Books and Teaching
+#   </h3>
+#   <ul>
+#    <li>
+#     The Complete iOS App Development Bootcamp
+#    </li>
+#    <li>
+#     The Complete Web Development Bootcamp
+#    </li>
+#    <li>
+#     100 Days of Code - The Complete Python Bootcamp
+#    </li>
+#   </ul>
+#   <hr/>
+#   <h3 class="heading">
+#    Other Pages
+#   </h3>
+#   <a href="https://angelabauer.github.io/cv/hobbies.html">
+#    My Hobbies
+#   </a>
+#   <a href="https://angelabauer.github.io/cv/contact-me.html">
+#    Contact Me
+#   </a>
+#  </body>
+# </html>
