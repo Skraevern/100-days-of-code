8d8e0700daf12172a39f7b36dea3af330e655356
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-10-22 13:51:06 +0200

    384

--- 047-day-047-amazon-price-tracker/main.py
+++ 047-day-047-amazon-price-tracker/main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import smtplib
 
 URL = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
 
@@ -21,3 +22,19 @@ soup = BeautifulSoup(contents, "html.parser")
 price = soup.find(name="span", class_="a-offscreen")
 price = float(price.string.replace("$", ""))
 print(price)
+
+# --- email alert
+
+
+gmail = "kristian.skreosen.test@gmail.com"  # smtp.gmail.com
+gmail_password = ""  # app password
+yahoo = "kristianskreosen.test@yahoo.com"  # smtp.mail.yahoo.com
+
+with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+    connection.starttls()
+    connection.login(user=gmail, password=gmail_password)
+    connection.sendmail(
+        from_addr=gmail,
+        to_addrs=yahoo,
+        msg=f"Subject:Rice Cooker Price\n\nPrice of rice cooker is now {price}",
+    )
