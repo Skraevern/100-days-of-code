1966a204a619e40fd6d4da618aaa135feae04f33
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-10-22 14:23:05 +0200

    386

--- 048-day-048-selenium-webdriver/386-setup-selenium.py
+++ 048-day-048-selenium-webdriver/386-setup-selenium.py
@@ -1,3 +1,11 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome()
+# Keep Chrome browser open after program finishes
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.amazon.com")
+
+driver.close()  # Close single tab
+driver.quit()  # Quit chrome
