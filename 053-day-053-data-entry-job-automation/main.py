788b0268da470103374727612c8a058765c47369
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-10-25 14:07:25 +0200

    git push

--- 053-day-053-data-entry-job-automation/main.py
+++ 053-day-053-data-entry-job-automation/main.py
@@ -5,23 +5,27 @@ FORM = "https://docs.google.com/forms/d/e/1FAIpQLSfXDSU2nctGXYA54x5OFpl2RFQklaAZ
 ZILLOW = "https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3Anull%2C%22mapBounds%22%3A%7B%22west%22%3A-122.56276167822266%2C%22east%22%3A-122.30389632177734%2C%22south%22%3A37.69261345230467%2C%22north%22%3A37.857877098316834%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%7D"
 
 
-header = {
-    "ACCEPT": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-    "ACCEPT-ENCODING": "gzip, deflate, br",
-    "ACCEPT-LANGUAGE": "en-US,en;q=0.5",
-    "SEC-FETCH-DEST": "document",
-    "SEC-FETCH-MODE": "navigate",
-    "SEC-FETCH-SITE": "cross-site",
-    "TE": "trailers",
-    "UPGRADE-INSECURE-REQUESTS": "1",
-    "USER-AGENT": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0",
+headers = {
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Origin": "https://www.zillow.com",
+    "Referer": "https://www.zillow.com/homes/for_rent/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22isMapVisible%22%3Afalse%2C%22mapBounds%22%3A%7B%22west%22%3A-122.62078322363281%2C%22east%22%3A-122.24587477636719%2C%22south%22%3A37.676990660829674%2C%22north%22%3A37.873461716438335%7D%2C%22mapZoom%22%3A11%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22price%22%3A%7B%22min%22%3Anull%2C%22max%22%3A872627%7D%2C%22mp%22%3A%7B%22min%22%3Anull%2C%22max%22%3A3000%7D%2C%22beds%22%3A%7B%22min%22%3A1%2C%22max%22%3Anull%7D%7D%2C%22isListVisible%22%3Atrue%7D",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
 }
 
-response = requests.get(f"{ZILLOW}", headers=header)
+response = requests.get(f"{ZILLOW}", headers=headers)
+with open("./website.html", "wb+") as file:
+    file.write(response.content)
 
+with open("./website.html", "r") as file:
+    contents = file.read()
 
-soup = BeautifulSoup(response.text, "html.parser")
-print(soup)
-prices = soup.find_all(name="b")
+soup = BeautifulSoup(contents, "html.parser")
+
+soup_prices = soup.find_all("span", attrs={"data-test": "property-card-price"})
+prices = []
+for soup in soup_prices:
+    prices.append(soup.text)
 
 print(prices)
