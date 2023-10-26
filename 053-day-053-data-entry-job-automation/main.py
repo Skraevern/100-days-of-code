54f00990d8967259c4e7ffdb1b16a7ece307aded
Skraevern <86933999+Skraevern@users.noreply.github.com>
2023-10-26 14:34:14 +0200

    Finished day 53 data entry automation

--- 053-day-053-data-entry-job-automation/main.py
+++ 053-day-053-data-entry-job-automation/main.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 FORM = "https://docs.google.com/forms/d/e/1FAIpQLSfXDSU2nctGXYA54x5OFpl2RFQklaAZCG8ctvgHFmi7BUDkKQ/viewform?usp=sf_link"
 ZILLOW = "https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3Anull%2C%22mapBounds%22%3A%7B%22west%22%3A-122.56276167822266%2C%22east%22%3A-122.30389632177734%2C%22south%22%3A37.69261345230467%2C%22north%22%3A37.857877098316834%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%7D"
@@ -14,6 +16,8 @@ headers = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
 }
 
+
+# --- bf4 --- #
 response = requests.get(f"{ZILLOW}", headers=headers)
 with open("./website.html", "wb+") as file:
     file.write(response.content)
@@ -39,3 +43,45 @@ links = list(dict.fromkeys(links))
 
 for adress in soup.find_all("address", attrs={"data-test": "property-card-addr"}):
     adresses.append(adress.text)
+
+
+# --- Selenium --- #
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get(FORM)
+
+for i in range(0, len(prices)):
+    try:
+        another_form = driver.find_element(
+            By.XPATH, value="/html/body/div[1]/div[2]/div[1]/div/div[4]/a"
+        )
+        another_form.click()
+    except:
+        pass
+    finally:
+        address_input = driver.find_element(
+            By.XPATH,
+            value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input',
+        )
+        price_input = driver.find_element(
+            By.XPATH,
+            value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input',
+        )
+        link_input = driver.find_element(
+            By.XPATH,
+            value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input',
+        )
+        send_btn = driver.find_element(
+            By.XPATH,
+            value='//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span',
+        )
+
+        address_input.send_keys(adresses[i])
+        price_input.send_keys(prices[i])
+        link_input.send_keys(links[i])
+        send_btn.click()
+
+driver.quit()
